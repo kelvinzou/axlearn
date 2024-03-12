203b2a26a42601850dccda6908323142b57431db
Kelvin Zou <xuan_zou@apple.com>
2024-03-11 19:48:09 -0700

    update the test

--- axlearn/common/flash_attention/gpu_attention_simple_test.py
+++ axlearn/common/flash_attention/gpu_attention_simple_test.py
@@ -33,7 +33,7 @@ from jax.experimental.pallas.ops.attention import mha as pallas_mha
     ],
 )
 @pytest.mark.parametrize("block_size", [128])
-@pytest.mark.parametrize("use_fwd", [True, False])
+@pytest.mark.parametrize("use_pallas", [True, False])
 @pytest.mark.parametrize("causal", [False])
 @pytest.mark.parametrize("sm_scale", [1.0])
 @pytest.mark.parametrize("bias_type", ["none"])
@@ -43,7 +43,7 @@ def test_fwd_against_ref(
     num_heads: int,
     per_head_dim: int,
     block_size: int,
-    use_fwd: bool,
+    use_pallas: bool,
     causal: bool,
     sm_scale: float,
     bias_type: str,
@@ -58,7 +58,7 @@ def test_fwd_against_ref(
     else:
         bias = None
 
-    if use_fwd:
+    if use_pallas:
         @jax.jit
         def impl(q, k, v, bias):
             fn = functools.partial(
@@ -89,67 +89,80 @@ def test_fwd_against_ref(
     chex.assert_trees_all_close(o, o_ref, atol=0.05)
 
 
-# @pytest.mark.parametrize(
-#     "batch_size,num_heads,seq_len,per_head_dim",
-#     [
-#         (2, 2, 384, 64),
-#     ],
-# )
-# @pytest.mark.parametrize("bias_type", ["none"])
-# @pytest.mark.parametrize("block_size", [128])
-# @pytest.mark.parametrize("causal", [False])
-# def test_bwd_against_ref(
-#     batch_size: int,
-#     num_heads: int,
-#     seq_len: int,
-#     per_head_dim: int,
-#     bias_type: str,
-#     block_size: int,
-#     causal: bool,
-# ):
-#     q = jax.random.normal(
-#         jax.random.PRNGKey(0), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
-#     )
-#     k = jax.random.normal(
-#         jax.random.PRNGKey(1), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
-#     )
-#     v = jax.random.normal(
-#         jax.random.PRNGKey(2), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
-#     )
+@pytest.mark.parametrize(
+    "batch_size,num_heads,seq_len,per_head_dim",
+    [
+        (2, 2, 384, 64),
+    ],
+)
+@pytest.mark.parametrize("bias_type", ["none"])
+@pytest.mark.parametrize("block_size", [128])
+@pytest.mark.parametrize("use_pallas", [True, False])
+@pytest.mark.parametrize("causal", [False])
+def test_bwd_against_ref(
+    batch_size: int,
+    num_heads: int,
+    seq_len: int,
+    per_head_dim: int,
+    bias_type: str,
+    block_size: int,
+    use_pallas: bool,
+    causal: bool,
+):
+    q = jax.random.normal(
+        jax.random.PRNGKey(0), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
+    k = jax.random.normal(
+        jax.random.PRNGKey(1), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
+    v = jax.random.normal(
+        jax.random.PRNGKey(2), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
 
-#     if bias_type == "matrix":
-#         bias = jax.random.normal(
-#             jax.random.PRNGKey(3), (batch_size, num_heads, seq_len, seq_len), dtype=jnp.float16
-#         )
-#     else:
-#         bias = None
+    if bias_type == "matrix":
+        bias = jax.random.normal(
+            jax.random.PRNGKey(3), (batch_size, num_heads, seq_len, seq_len), dtype=jnp.float16
+        )
+    else:
+        bias = None
 
-#     assert str(q.device()) == "cuda:0"
-#     sm_scale = q.shape[-1] ** -0.5
+    assert str(q.device()) == "cuda:0"
+    sm_scale = q.shape[-1] ** -0.5
 
-#     # Compare outputs.
-#     # jax_out = flash_attention(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
-#     jax_out = pallas_mha(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
-#     jax_ref_out = mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
-#     chex.assert_trees_all_close(jax_out, jax_ref_out, atol=0.005)
+    # Compare outputs.
+    # jax_out = flash_attention(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
+    jax_out = pallas_mha(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
+    jax_ref_out = mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale)
+    chex.assert_trees_all_close(jax_out, jax_ref_out, atol=0.005)
+    if use_pallas:
+        def fn(q, k, v, bias):
+            # return flash_attention(
+            return pallas_mha(
+                q,
+                k,
+                v,
+                None,
+                causal=causal,
+                sm_scale=sm_scale,
+                block_q=block_size,
+                block_k=block_size,
+            ).sum()
+    else:
+        def fn(q, k, v, bias):
+            return flash_attention(
+                q,
+                k,
+                v,
+                bias,
+                causal=causal,
+                softmax_scale=sm_scale,
+                block_q=block_size,
+                block_k=block_size,
+            ).sum()
+    def ref_fn(q, k, v, bias):
+        return mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale).sum()
 
-#     def fn(q, k, v, bias):
-#         # return flash_attention(
-#         return pallas_mha(
-#             q,
-#             k,
-#             v,
-#             bias,
-#             causal=causal,
-#             sm_scale=sm_scale,
-#             block_q=block_size,
-#             block_k=block_size,
-#         ).sum()
-
-#     def ref_fn(q, k, v, bias):
-#         return mha_reference(q, k, v, bias, causal=causal, softmax_scale=sm_scale).sum()
-
-#     # Compare gradients.
-#     jax_grads = jax.grad(fn, argnums=(0, 1, 2))(q, k, v, bias)
-#     jax_ref_grads = jax.grad(ref_fn, argnums=(0, 1, 2))(q, k, v, bias)
-#     chex.assert_trees_all_close(jax_grads, jax_ref_grads, atol=0.05)
+    # Compare gradients.
+    jax_grads = jax.grad(fn, argnums=(0, 1, 2))(q, k, v, bias)
+    jax_ref_grads = jax.grad(ref_fn, argnums=(0, 1, 2))(q, k, v, bias)
+    chex.assert_trees_all_close(jax_grads, jax_ref_grads, atol=0.05)
